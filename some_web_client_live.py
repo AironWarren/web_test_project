fa2bf5d0ae62f408967b2ab8ba138b1c13999096
Лысов Максим <lysov.m@future-tech.ru>
2022-12-27 17:12:21 +0700

    json

--- some_web_client_live.py
+++ some_web_client_live.py
@@ -1,14 +1,23 @@
 import requests
+import json
+from datetime import datetime
 
 
 class SomeResourceClient:
     def __init__(self, url):
         self.url = url
 
-    def user_get_status(self, user_id):
-        resp = requests.get(f"{self.url}/web/user/get-status/{user_id}")
-        c = 1
+    def __user_get_status(self, user_id):
+        resp = requests.get(f"{self.url}/clickstream/events/1/{user_id}")
+        json_data = json.loads(resp.text)
+        return json_data
+
+    def get_user_last_action_time(self, user_id):
+        json_data = self.user_get_status(user_id)
+        last_action_time = json_data["result"]
+        time_diff = json_data["timeDiff"]
+        return datetime.fromtimestamp(last_action_time - time_diff)
 
 
-some_resource_client = SomeResourceClient("https://www.avito.ru")
-some_resource_client.user_get_status(20405678)
+some_resource_client = SomeResourceClient("https://cs.avito.ru")
+some_resource_client.user_get_status("json")
