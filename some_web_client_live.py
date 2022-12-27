e33c8a6be7487da1933b4972433420b9593937d3
Лысов Максим <lysov.m@future-tech.ru>
2022-12-27 15:32:36 +0700

    Error ID

--- some_web_client_live.py
+++ some_web_client_live.py
@@ -1,9 +1,14 @@
 import requests
 
 
-c = 1
-
 class SomeResourceClient:
-    
-res = requests.get('https://content.adriver.ru/AdRiverFPS.js')
-print(res.text)
+    def __init__(self, url):
+        self.url = url
+
+    def user_get_status(self, user_id):
+        resp = requests.get(f"{self.url}/web/user/get-status/{user_id}")
+        c = 1
+
+
+some_resource_client = SomeResourceClient("https://www.avito.ru")
+some_resource_client.user_get_status(20405678)
